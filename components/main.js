21777754309ae0aafd0385b24dcd7b4d0a07bfb1
okaaay <vano_msk@mail.ru>
2024-11-10 20:40:21 +0300

    Final test

--- components/main.js
+++
@@ -1,7 +0,0 @@
-function main() { // Вы здесь просто ОБЪЯВИЛИ функцию.
-    alert("This!");
-  }
-  main();
-document.getElementById('button2').addEventListener('click',function() {
-    console.log('jhnfjhi');
-})
