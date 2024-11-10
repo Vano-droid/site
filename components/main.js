5747167331454da2d91ee49a64480c9e3f97a991
okaaay <vano_msk@mail.ru>
2024-11-10 16:26:53 +0300

    @#

---
+++ components/main.js
@@ -0,0 +1,10 @@
+var isMonthly=true;
+const button=document.getElementsByClassName("pricing6-button10 thq-button-filled");
+const button2=document.getElementsByClassName("pricing6-button11 thq-button-outline")
+const button3=document.getElementsByClassName("pricing6-button12 thq-button-outline");
+const button4=document.getElementsByClassName("pricing6-button13 thq-button-filled");
+const container=document.getElementsByClassName("pricing6-container1");
+const container2=document.getElementsByClassName("pricing6-container2");
+if (isMonthly=true){
+    container.style.display="none";
+}
