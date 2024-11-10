8d365c77408b8a6b054b486168590bebbcfd69c8
okaaay <vano_msk@mail.ru>
2024-11-10 17:23:27 +0300

    dwa

--- components/main.js
+++ components/main.js
@@ -1,7 +1,23 @@
 var isMonthly=true;
-const button=document.getElementsByClassName("pricing6-button10 thq-button-filled");
-const button2=document.getElementsByClassName("pricing6-button11 thq-button-outline")
-const button3=document.getElementsByClassName("pricing6-button12 thq-button-outline");
-const button4=document.getElementsByClassName("pricing6-button13 thq-button-filled");
-const container=document.getElementsByClassName("pricing6-container1");
-const container2=document.getElementsByClassName("pricing6-container2");
+let button=document.getElementsByClassName("pricing6-button10 thq-button-filled");
+let button2=document.getElementsByClassName("pricing6-button11 thq-button-outline")
+let button3=document.getElementsByClassName("pricing6-button12 thq-button-outline");
+let button4=document.getElementsByClassName("pricing6-button13 thq-button-filled");
+let container=document.getElementsByClassName("pricing6-container1");
+let container2=document.getElementsByClassName("pricing6-container2");
+button2.addEventListener('click',function(){
+    button2.style.display="flex";
+    button4.style.display="flex";
+    container2.style.display="flex";
+    button.style.display="none";
+    button3.style.display="none";
+    container.style.display="none";
+})
+button3.addEventListener('click',function(){
+    button2.style.display="none";
+    button4.style.display="none";
+    container2.style.display="none";
+    button.style.display="flex";
+    button3.style.display="flex";
+    container.style.display="flex";
+})
