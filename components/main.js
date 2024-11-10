d654bdbcbdf8b41388ac6e4968b9afbae5f01c58
okaaay <vano_msk@mail.ru>
2024-11-10 17:28:25 +0300

    @#

--- components/main.js
+++ components/main.js
@@ -6,18 +6,18 @@ let button4=document.getElementsByClassName("pricing6-button13 thq-button-filled
 let container=document.getElementsByClassName("pricing6-container1");
 let container2=document.getElementsByClassName("pricing6-container2");
 button2.addEventListener('click',function(){
-    button2.style.display="flex";
-    button4.style.display="flex";
-    container2.style.display="flex";
-    button.style.display="none";
-    button3.style.display="none";
-    container.style.display="none";
-})
-button3.addEventListener('click',function(){
     button2.style.display="none";
     button4.style.display="none";
     container2.style.display="none";
     button.style.display="flex";
     button3.style.display="flex";
     container.style.display="flex";
+})
+button3.addEventListener('click',function(){
+    button2.style.display="flex";
+    button4.style.display="flex";
+    container2.style.display="flex";
+    button.style.display="none";
+    button3.style.display="none";
+    container.style.display="none";
 })
